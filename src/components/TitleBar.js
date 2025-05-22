3f05f852e6a85c339ac07c23b48a55df9c6f2c63
liminalFrog <adam@trinity.tel>
2025-05-21 21:21:47 -0500

    UI Changes

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Navbar, Nav, Button, Dropdown } from 'react-bootstrap';
 import path from 'path-browserify';
 import './TitleBar.css';
+import { CgMathMinus, CgMinimize, CgMaximize, CgClose } from 'react-icons/cg';
 
 const electron = window.electron;
 const ipcRenderer = electron ? electron.ipcRenderer : null;
@@ -277,19 +278,17 @@ function TitleBar({ currentFile, dirty }) {
       </div>
       <div className="window-controls">
         <Button variant="link" className="window-control minimize" onClick={handleMinimize}>
-          <svg width="12" height="12" viewBox="0 0 12 12">
-            <rect fill="currentColor" width="10" height="1" x="1" y="6"></rect>
-          </svg>
+          <CgMathMinus />
         </Button>
         <Button variant="link" className="window-control maximize" onClick={handleMaximize}>
-          <svg width="12" height="12" viewBox="0 0 12 12">
-            <rect width="9" height="9" x="1.5" y="1.5" fill="none" stroke="currentColor"></rect>
-          </svg>
+          {ipcRenderer && ipcRenderer.send('window-maximize') ? (
+            <CgMinimize />
+          ) : (
+            <CgMaximize />
+          )}
         </Button>
         <Button variant="link" className="window-control close" onClick={handleClose}>
-          <svg width="12" height="12" viewBox="0 0 12 12">
-            <path stroke="currentColor" strokeWidth="1.1" d="M3,3 L9,9 M9,3 L3,9"></path>
-          </svg>
+          <CgClose />
         </Button>
       </div>
     </div>
