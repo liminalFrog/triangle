913dedf6e8540719bb39c69725d047d6417b9c29
liminalFrog <adam@trinity.tel>
2025-05-22 05:36:16 -0500

    Fixed titlebar focus issue

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Navbar, Nav, Button, Dropdown } from 'react-bootstrap';
 import path from 'path-browserify';
 import './TitleBar.css';
@@ -9,7 +9,48 @@ const ipcRenderer = electron ? electron.ipcRenderer : null;
 
 function TitleBar({ currentFile, dirty }) {
   const [activeDropdown, setActiveDropdown] = useState(null);
+  const menuRef = useRef(null);
 
+  // Clear focus when clicking outside menu
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setActiveDropdown(null);
+        // Ensure all focused elements are blurred
+        if (document.activeElement) {
+          document.activeElement.blur();
+        }
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  // Helper function to handle menu item selection and focus cleanup
+  const handleMenuItemSelection = (callback) => {
+    return () => {
+      // Execute the callback function
+      callback();
+      
+      // Close the dropdown
+      setActiveDropdown(null);
+      
+      // Remove focus from any active element
+      if (document.activeElement) {
+        document.activeElement.blur();
+      }
+      
+      // Set focus to the main container to ensure no menu items remain focused
+      if (menuRef.current) {
+        menuRef.current.focus();
+      }
+    };
+  };
+
+  // Handling window controls
   const handleMinimize = () => {
     if (ipcRenderer) {
       ipcRenderer.send('window-minimize');
@@ -37,93 +78,116 @@ function TitleBar({ currentFile, dirty }) {
       return currentFile.split(/[\\/]/).pop() + (dirty ? ' *' : '');
     }
   };
-
-  // Menu handlers
-  const handleNewFile = () => {
+  
+  // Base menu action handlers
+  const baseHandleNewFile = () => {
     if (ipcRenderer) {
       ipcRenderer.send('menu-new');
     }
   };
 
-  const handleOpenFile = () => {
+  const baseHandleOpenFile = () => {
     if (ipcRenderer) {
       ipcRenderer.send('menu-open');
     }
   };
 
-  const handleSaveFile = () => {
+  const baseHandleSaveFile = () => {
     if (ipcRenderer) {
       ipcRenderer.send('menu-save');
     }
   };
 
-  const handleSaveAsFile = () => {
+  const baseHandleSaveAsFile = () => {
     if (ipcRenderer) {
       ipcRenderer.send('menu-save-as');
     }
   };
 
-  const handleExit = () => {
+  const baseHandleExit = () => {
     if (ipcRenderer) {
       ipcRenderer.send('menu-exit');
     }
   };
 
-  const handleUndo = () => {
+  const baseHandleUndo = () => {
     // Implement undo functionality
     console.log('Undo');
   };
 
-  const handleRedo = () => {
+  const baseHandleRedo = () => {
     // Implement redo functionality
     console.log('Redo');
   };
 
-  const handlePreferences = () => {
+  const baseHandlePreferences = () => {
     // Open preferences dialog
     console.log('Preferences');
   };
 
-  const handleFullscreen = () => {
+  const baseHandleFullscreen = () => {
     if (ipcRenderer) {
       ipcRenderer.send('toggle-fullscreen');
     }
   };
 
-  const handleTogglePerspective = () => {
+  const baseHandleTogglePerspective = () => {
     // Toggle between perspective and orthographic camera
     console.log('Toggle Perspective/Orthographic');
   };
 
-  const handleSaveCameraPosition = (position) => {
+  const baseHandleSaveCameraPosition = (position) => {
     // Save camera position at slot 0-9
     console.log(`Save camera position ${position}`);
   };
 
-  const handleAddObject = (objectType) => {
+  const baseHandleAddObject = (objectType) => {
     // Add object to scene
     console.log(`Add ${objectType}`);
   };
 
-  const handleRender = () => {
+  const baseHandleRender = () => {
     // Render the scene
     console.log('Render');
   };
 
-  const handleRenderSettings = () => {
+  const baseHandleRenderSettings = () => {
     // Open render settings
     console.log('Render Settings');
   };
 
-  const handleHelp = () => {
+  const baseHandleHelp = () => {
     // Open help documentation
     console.log('Help');
   };
 
-  const handleAbout = () => {
+  const baseHandleAbout = () => {
     // Show about dialog
     console.log('About Triangle');
   };
+
+  // Wrapped handlers with focus management
+  const handleNewFile = handleMenuItemSelection(baseHandleNewFile);
+  const handleOpenFile = handleMenuItemSelection(baseHandleOpenFile);
+  const handleSaveFile = handleMenuItemSelection(baseHandleSaveFile);
+  const handleSaveAsFile = handleMenuItemSelection(baseHandleSaveAsFile);
+  const handleExit = handleMenuItemSelection(baseHandleExit);
+  const handleUndo = handleMenuItemSelection(baseHandleUndo);
+  const handleRedo = handleMenuItemSelection(baseHandleRedo);
+  const handlePreferences = handleMenuItemSelection(baseHandlePreferences);
+  const handleFullscreen = handleMenuItemSelection(baseHandleFullscreen);
+  const handleTogglePerspective = handleMenuItemSelection(baseHandleTogglePerspective);
+  const handleRender = handleMenuItemSelection(baseHandleRender);
+  const handleRenderSettings = handleMenuItemSelection(baseHandleRenderSettings);
+  const handleHelp = handleMenuItemSelection(baseHandleHelp);
+  const handleAbout = handleMenuItemSelection(baseHandleAbout);
+  
+  // For functions with parameters, we need to create closures
+  const handleSaveCameraPosition = (position) => 
+    handleMenuItemSelection(() => baseHandleSaveCameraPosition(position))();
+  
+  const handleAddObject = (objectType) => 
+    handleMenuItemSelection(() => baseHandleAddObject(objectType))();
 
   return (
     <div className="title-bar" data-bs-theme="dark">
@@ -132,14 +196,21 @@ function TitleBar({ currentFile, dirty }) {
           <img src="/triangle-icon.png" alt="Triangle" className="title-bar-icon" />
           <span>Triangle</span>
         </div>
-        <Navbar variant="dark" className="custom-navbar">
-          <Nav className="me-auto">
-            <Dropdown
+        <Navbar variant="dark" className="custom-navbar" ref={menuRef} tabIndex="-1">
+          <Nav className="me-auto">            <Dropdown
               show={activeDropdown === 'file'}
-              onToggle={(isOpen) => setActiveDropdown(isOpen ? 'file' : null)}
+              onToggle={(isOpen) => {
+                setActiveDropdown(isOpen ? 'file' : null);
+                if (!isOpen) {
+                  // Force blur when dropdown closes
+                  if (document.activeElement) {
+                    document.activeElement.blur();
+                  }
+                }
+              }}
               align="start"
             >
-              <Dropdown.Toggle as={Nav.Link} id="dropdown-file">
+              <Dropdown.Toggle as={Nav.Link} id="dropdown-file" className="menu-toggle">
                 File
               </Dropdown.Toggle>
               <Dropdown.Menu>
@@ -160,14 +231,19 @@ function TitleBar({ currentFile, dirty }) {
                   Exit <span className="shortcut">Ctrl+Q</span>
                 </Dropdown.Item>
               </Dropdown.Menu>
-            </Dropdown>
-
-            <Dropdown
+            </Dropdown>            <Dropdown
               show={activeDropdown === 'edit'}
-              onToggle={(isOpen) => setActiveDropdown(isOpen ? 'edit' : null)}
+              onToggle={(isOpen) => {
+                setActiveDropdown(isOpen ? 'edit' : null);
+                if (!isOpen) {
+                  if (document.activeElement) {
+                    document.activeElement.blur();
+                  }
+                }
+              }}
               align="start"
             >
-              <Dropdown.Toggle as={Nav.Link} id="dropdown-edit">
+              <Dropdown.Toggle as={Nav.Link} id="dropdown-edit" className="menu-toggle">
                 Edit
               </Dropdown.Toggle>
               <Dropdown.Menu>
@@ -182,14 +258,19 @@ function TitleBar({ currentFile, dirty }) {
                   Preferences
                 </Dropdown.Item>
               </Dropdown.Menu>
-            </Dropdown>
-
-            <Dropdown
+            </Dropdown>            <Dropdown
               show={activeDropdown === 'view'}
-              onToggle={(isOpen) => setActiveDropdown(isOpen ? 'view' : null)}
+              onToggle={(isOpen) => {
+                setActiveDropdown(isOpen ? 'view' : null);
+                if (!isOpen) {
+                  if (document.activeElement) {
+                    document.activeElement.blur();
+                  }
+                }
+              }}
               align="start"
             >
-              <Dropdown.Toggle as={Nav.Link} id="dropdown-view">
+              <Dropdown.Toggle as={Nav.Link} id="dropdown-view" className="menu-toggle">
                 View
               </Dropdown.Toggle>
               <Dropdown.Menu>
@@ -206,14 +287,19 @@ function TitleBar({ currentFile, dirty }) {
                   </Dropdown.Item>
                 ))}
               </Dropdown.Menu>
-            </Dropdown>
-
-            <Dropdown
+            </Dropdown>            <Dropdown
               show={activeDropdown === 'add'}
-              onToggle={(isOpen) => setActiveDropdown(isOpen ? 'add' : null)}
+              onToggle={(isOpen) => {
+                setActiveDropdown(isOpen ? 'add' : null);
+                if (!isOpen) {
+                  if (document.activeElement) {
+                    document.activeElement.blur();
+                  }
+                }
+              }}
               align="start"
             >
-              <Dropdown.Toggle as={Nav.Link} id="dropdown-add">
+              <Dropdown.Toggle as={Nav.Link} id="dropdown-add" className="menu-toggle">
                 Add
               </Dropdown.Toggle>
               <Dropdown.Menu>
@@ -235,14 +321,19 @@ function TitleBar({ currentFile, dirty }) {
                   Window
                 </Dropdown.Item>
               </Dropdown.Menu>
-            </Dropdown>
-
-            <Dropdown
+            </Dropdown>            <Dropdown
               show={activeDropdown === 'render'}
-              onToggle={(isOpen) => setActiveDropdown(isOpen ? 'render' : null)}
+              onToggle={(isOpen) => {
+                setActiveDropdown(isOpen ? 'render' : null);
+                if (!isOpen) {
+                  if (document.activeElement) {
+                    document.activeElement.blur();
+                  }
+                }
+              }}
               align="start"
             >
-              <Dropdown.Toggle as={Nav.Link} id="dropdown-render">
+              <Dropdown.Toggle as={Nav.Link} id="dropdown-render" className="menu-toggle">
                 Render
               </Dropdown.Toggle>
               <Dropdown.Menu>
@@ -253,14 +344,19 @@ function TitleBar({ currentFile, dirty }) {
                   Render Settings
                 </Dropdown.Item>
               </Dropdown.Menu>
-            </Dropdown>
-
-            <Dropdown
+            </Dropdown>            <Dropdown
               show={activeDropdown === 'help'}
-              onToggle={(isOpen) => setActiveDropdown(isOpen ? 'help' : null)}
+              onToggle={(isOpen) => {
+                setActiveDropdown(isOpen ? 'help' : null);
+                if (!isOpen) {
+                  if (document.activeElement) {
+                    document.activeElement.blur();
+                  }
+                }
+              }}
               align="start"
             >
-              <Dropdown.Toggle as={Nav.Link} id="dropdown-help">
+              <Dropdown.Toggle as={Nav.Link} id="dropdown-help" className="menu-toggle">
                 Help
               </Dropdown.Toggle>
               <Dropdown.Menu>
